59e6a0bc0ba12ec36ffdf8e96d7c7dfd2e7e2ca2
Lehi <w0rldend3r@gmail.com>
2026-03-16 20:40:02 -0600

    Limpieza de credenciales por seguridad

--- esp32_code/sketch_mar16a.ino
+++ esp32_code/sketch_mar16a.ino
@@ -3,10 +3,10 @@
 
 // --- DATOS DE CONEXIÓN ---
 #define WIFI_SSID "TP-Link_B770"
-#define WIFI_PASSWORD "20648367"
+#define WIFI_PASSWORD "TU-CLAVE-WIFI"  // Reemplaza con tu contraseña Wi-Fi
 #define FIREBASE_HOST "https://database-16ba6-default-rtdb.firebaseio.com" 
-#define API_KEY "AIzaSyAL4PnFvD-4k-M4ea9ZfllJw0pZkvsZHmI"
-#define FIREBASE_SECRET "hrMizAkYKtPdbMsLiTlePI1VrVYQ1WtetgkqSO1v"
+#define API_KEY "TU-API-KEY"  // Reemplaza con tu API Key de Firebase
+#define FIREBASE_SECRET "TU-FIREBASE-SECRET"  // Reemplaza con tu Firebase Secret
 
 FirebaseData fbdo;
 FirebaseAuth auth;
